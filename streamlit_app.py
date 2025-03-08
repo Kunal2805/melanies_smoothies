bbfbadc6585da0269179b8a3a104381269edbe75
Kunal2805 <khot.kunal28@gmail.com>
2025-03-09 01:54:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()   
-Convert Data frames in pandas
+#Convert Data frames in pandas
 pd_df=my_datafram.to_pandas()
 
                                                                                             
